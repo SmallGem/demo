a63371104afd29bfb1b5c21582ec0a2cd4b3286d
SmallGem <961781110@qq.com>
2019-01-11 00:47:23 +0800

    Make request

--- application/controller/request_controller.py
+++ application/controller/request_controller.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 import requests
+
+from flask import current_app
+
+url_prefix = current_app().config['API_URL']
+
+
+def get(url_shard):
+
+    url = url_prefix + url_shard
+    result = requests.get(url)
+
+    return result
+
+
+def post(url_shard, data):
+
+    url = url_prefix + url_shard
+    result = requests.post(url, data)
