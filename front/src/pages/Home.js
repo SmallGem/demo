74b050b7e06e5a783c8c4bc2214eaeb7b0e910fc
Tricker <tricker.pan@gmail.com>
2019-01-17 14:31:24 +0800

    Add chart.js in front/pages/Home

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'
+import React, {Component} from "react"
+import Chart from "chart.js/dist/Chart.bundle.min";
 
 class Home extends Component {
+    componentDidMount() {
+        const ctx = document.getElementById("exampleChart");
+        const datasets = [{
+            label: "# of Sales Volume",
+            data: [12, 19, 3, 0, 2, 3, 16],
+            backgroundColor: "rgba(255, 159, 64, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+            lineTension: 0,
+            pointBackgroundColor: "rgba(153, 102, 255, 0.2)",
+            pointBorderColor: "rgba(75, 192, 192, 0.2)",
+            PointBorderWidth: 1,
+        }];
+        const chart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: ["Sum", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"],
+                datasets: datasets
+            }
+        });
+    }
+
     render() {
         return (
-            <div>
-                <h1>Something in here.</h1>
+            <div className="tile is-ancestor">
+                <canvas id="exampleChart"/>
             </div>
         )
     }
